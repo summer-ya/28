fbba9062f351005a772c9476e782aa3cf098b232
JI HYEON LEE <leejh.bjkrst@gmail.com>
2022-07-02 11:25:20 +0900

    28

---
+++ src/exept/Ex8_4.java
@@ -0,0 +1,21 @@
+package exept;
+
+public class Ex8_4 {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		try {
+			System.out.println(3);
+			System.out.println(0/0);
+			System.out.println(4);
+		} catch (ArithmeticException ae) {
+			if(ae instanceof ArithmeticException)
+				System.out.println("true");
+			System.out.println("ArithmeticException");
+		} catch (Exception e) {
+			System.out.println("Exception");
+		} // try-catch³¡
+		System.out.println(6);
+	} //main³¡
+
+}
